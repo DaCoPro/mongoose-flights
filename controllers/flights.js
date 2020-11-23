765a3dd41d0e264f0cb4ddef0738d939367ddbb3
David Comer <davidcomer@Davids-MacBook-Pro.local>
2020-11-22 19:25:52 -0800

    Add form for new flights.

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,10 +2,15 @@ const Flight = require('../model/flight');
 
 module.exports = {
     index,
+    new: newFlight,
+
 }
 
 function index (req, res) {
     Flight.find({}, function (err, flights) {
         res.render('flights/index', { flights });
     });
+}
+function newFlight(req, res) {
+    res.render('flights/new');
 }
--- routes/flights.js
+++ routes/flights.js
@@ -5,5 +5,6 @@ var flightsCtrl = require('../controllers/flights')
 
 /* GET users listing. */
 router.get('/', flightsCtrl.index);
+router.get('/new', flightsCtrl.new);
 
 module.exports = router;
